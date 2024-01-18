dec0b717fff0891b5d1b30c128fe96d82e05e876
meriemfed <154727592+meriemfed@users.noreply.github.com>
2024-01-18 20:57:37 +0100

    additions des fonctions utilisees

--- LOVC/insertion.c
+++ LOVC/insertion.c
@@ -171,3 +171,145 @@ void AllocBloc(Fichier* fichier)
     Aff_entete(fichier,4,Entete(fichier,4)+1); //Incrémenter le nombre total de blocs de la liste
     Aff_entete(fichier,5,Entete(fichier,4)); //Mettre à jour le champ qui correspond à l'adresse du dernier bloc de la liste
 }
+
+//Outils utilisés
+char* ChaineAlea(int taille)
+{
+    char* alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,.-'?!"; //Une chaine qui contient tous les caractères possibles
+    char* str = NULL; //La chaine aléatoire qu'on va générer
+    str = (char*)malloc(sizeof(char)*(taille+2)); //Allouer de l'espace mémoire pour cette chaine
+    for(int i=0;i<taille;i++)
+    {
+        str[i] = alpha[rand()%68]; //Choisir un caractère aléatoirement
+    }
+    str[taille] = '#'; //Ajouter un sépérateur
+    str[taille] = '\0'; //Un caractère vide pour déterminer la fin de la chaine de caractères
+    return str; //Retouner la chaine aléatoire
+}
+
+//Initialiser les enregistrements avec des séparateurs
+void InitialiserDonnee(Donnee* d)
+{
+    strcpy(d->numero,"|||||||||");
+    d->taille[0] = '0';
+    for(int i=1;i<NB_TAILLE;i++)
+        d->taille[i] = '#';
+    d->taille[NB_TAILLE-1] = '\0';
+    d->eff = '0';
+}
+
+//Concatène tous les champs de l'enregistrement en une chaine de caractères
+char* ConcatDonnee(Donnee d)
+{
+    char* str;
+    str = (char*)malloc(sizeof(char)*(37+NB_TAILLE+strlen(d.data))); //Alloue l'espace mémoire pour la chaine de caractères
+    int j=0;
+    strcpy(str, d.taille);
+    j += strlen(d.taille);
+    str[j] = '#';
+    str[j + 1] = d.eff;
+    j += 2;
+    str[j] = '#';
+    strcpy(str + j + 1, d.numero);
+    j += strlen(d.numero) + 1;
+    str[j] = '#';
+    strcpy(str + j + 1, d.data);
+    j += strlen(d.data) + 1;
+    str[j] = '#';
+    str[j+1] = '$';
+    str[j+2] = '\0';
+    return str; //Retourner la chaine conacténée
+}
+
+//Recupère le champ de l'enregistrement à partir du fichier
+void RecupChamp(Fichier* fichier,int n,Buffer* buf,int* i,int* j,char* donnee)
+{
+    int k = 0;
+    
+    while(buf->tab[*j] != '#')
+    {
+        //printf("\nbbb %s\n", buf->tab);
+        if(*j>=sizeof(buf->tab)) //En cas où le champ est divisé sur plus d'un bloc
+        {   
+            (*i)+=1;
+            *j = 0; //Rénitiliser le j à 0 (au début du nouveau bloc)
+            LireDir(fichier,*i,buf); //Lire le bloc suivant
+        }
+        
+        donnee[k] = buf->tab[*j];
+        
+        (*j)+=1;
+        k+=1;
+    }
+    (*j)+=1;
+    if(buf->tab[*j] == '$')
+        (*j)+=1;
+    donnee[k] = '\0';
+}
+
+
+//Récupère la chaine du fichier comme elle est sans enlever les séparateurs
+void RecupChaine(Fichier* fichier,int n,Buffer* buf,int* i,int* j,char donnee[])
+{
+    int k = 0;
+    while(buf->tab[*j] != '#')
+    {
+        
+        if(*j>=sizeof(buf->tab)) //En cas où la chaine est divisée sur plus d'un bloc
+        {
+            (*i)+=1;
+            *j = 0; //Rénitiliser le j à 0 (au début du nouveau bloc)
+            LireDir(fichier,*i,buf);
+        }
+        //printf("\nbbb %c\n", buf->tab[*j]);
+        donnee[k] = buf->tab[*j];
+        (*j)+=1;
+        k+=1;
+    }
+    (*j)+=1;
+    if(buf->tab[*j] == '$')
+        (*j)+=1;
+    if(buf->tab[*j] == '#')
+        (*j)+=1;
+    donnee[k] = '\0';
+}
+
+
+//Ecrie la chaine de caractères de taille n à la fin d'un fichier à partir de l'adresse i et la position j
+void EcrireChaine(Fichier* fichier,char* nom_physique,int n,Buffer* buf,int* i,int* j,char donnee[])
+{
+    int k = 0;
+    Ouvrir(fichier,nom_physique,'A'); //Ouvrir le fichier en mode ancien
+    LireDir(fichier,*i,buf); //Lire le bloc d'adresse i
+    while(k<n) //Tant que la chaine n'est pas finie
+    {
+        if(*j>=B) //Si la chaine dépasse la capacité du bloc
+        {
+            EcrireDir(fichier,*i,buf); //Ecrire le bloc à l'adresse i
+            AllocBloc(fichier); //Allouer un nouveau bloc à la fin du fichier et le chainer
+            *i = Entete(fichier,5); //Mettre à jour i à l'adresse de la queue de la liste
+            *j = 0; //Réinitialiser j à 0
+            LireDir(fichier,*i,buf); //Lire le nouveau bloc
+        }
+        buf->tab[*j] = donnee[k]; //Mettre à jour un caractère du buffer avec un caractère de la chaine qu'on veut écrire
+        (*j)+=1;
+        k+=1;
+    }
+    donnee[k] = '\0';
+    EcrireDir(fichier,*i,buf); //Ecrire le bloc à l'adresse i
+    Aff_entete(fichier,6,*j); //Mettre à jour la dernière position libre dans la queue
+    Aff_entete(fichier,2,Entete(fichier,2)+n); //Mettre à jour le nombre de caractères insérés
+    Fermer(fichier); //Fermer le fichier
+}
+
+char* EntrerDonnee(int numero)
+{
+    Donnee d; //Déclarer d comme enregistrement
+    InitialiserDonnee(&d); //Initialiser les champs de d
+    sprintf(d.numero,"%d",numero); //Affecter numero(entier) à d.numero(chaine de caractères)
+    printf("\t\t\tEntrez data (sans espaces): ");
+    d.data = (char*)malloc(sizeof(char)*250);
+    scanf("%s",d.data); //Lire le champ observation
+    sprintf(d.taille,"%d",NB_TAILLE+1+35+strlen(d.data));
+    return ConcatDonnee(d); //Retouner les champs concaténés
+}
