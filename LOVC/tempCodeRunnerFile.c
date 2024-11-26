d5c8233e54e508204c4c97fcb99f0b7538ed3b36
Haniaa567 <154731128+Haniaa567@users.noreply.github.com>
2024-11-26 13:50:22 +0100

    .

--- LOVC/tempCodeRunnerFile.c
+++ LOVC/tempCodeRunnerFile.c
@@ -1,1 +1,55 @@
-NB_TAILLE
+void SuppressionLogiqueLOVC(Fichier* fichier,char* nom_physique,int numero)
+{
+    int trouv;
+    int i,j;
+   
+    RechercheLOVC(fichier,nom_physique,numero,&i,&j,&trouv, NULL); //On effectue d'abord une recherche
+    if(trouv == 1) //Si le livret a été trouvé
+    {
+        int nb_taille;
+        Buffer buf;
+        char taille[NB_TAILLE];
+        Ouvrir(fichier,nom_physique,'A'); //On ouvre le fichier en mode ancien , il existe deja
+        LireDir(fichier,i,&buf); //lire le bloc dans buf
+        RecupChamp(fichier,NB_TAILLE,&buf,&i,&j,taille);  //On récupère le champ taille
+        nb_taille = atoi(taille); //On le convertit en entier
+        // cas 1 : la position du champ eff est dans le bloc courant
+        if(j<B) 
+        {
+            buf.tab[j] = '1'; //On met à jour le champ à 1 ce qui signifie que la donnée a été effacé
+            EcrireDir(fichier,i,&buf); //Ecrire le bloc
+        }
+        // cas 2 : la position du champ eff est dans le bloc suivant
+        else 
+        {
+            i = buf.suivant; //On passe au bloc suivant
+            if(i == -1) //S'il n'existe pas c'est une erreur de conception dans la strcture
+            {
+                printf("\t\t\tERREUR: Le champ efface n'existe pas\n");
+            }
+            else
+            {
+                LireDir(fichier,i,&buf); //On lie ce bloc
+                buf.tab[B-j] = '1'; //On met à jour le champ à 1 ce qui signifie que la donnée a été effacé
+                EcrireDir(fichier,i,&buf); //On écrit le bloc
+            }
+        }
+        Aff_entete(fichier,3,Entete(fichier,3)+nb_taille); //On met à jour le champ (nombre de caractères supprimés) de l'entête
+    }
+    else //Si le livret n'a pas été trouvé
+    {
+        printf("\t\t\tLe livret que vous voulez supprimer n'existe pas\n");
+    }
+
+    /*if(automatic == 1) //Si la réorganisation automatique était activée
+    {
+        Fichier fichier2;
+        if((float)Entete(fichier,3)/(float)Entete(fichier,2)>=seuil) //Si le seuil a été atteint ((nombre de caractères supprimés/nombre de caractères insérés)>=seuil)
+        {
+            Reorganisation(fichier,&fichier2,nom_physique,"Reo_Livrets_National.bin");
+            printf("\t\t\tReorganisation effectuee !\n");
+        }
+    }*/
+    Fermer(fichier);
+}
+
--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-# SFSD
- repository de l'examen TP LOVC 
- l'ancienne structure (insertion,recherche,suppression logique et visualisation gtk)
+Guide d'installation et d'exécution:
+Ce guide explique comment exécuter le programme de visualisation de structure de fichiers de type LOVC(ajouter, rechercher, supprimer, créer plusieurs enregistrements pour une structure LOVC avec GTK).
+Prérequis
+Bibliothèques nécessaires :
+GTK (GTK 3 ).
+Compilateur GCC.
+Compiler le programme
+la version finale de l'interface ce trouve dans le fichier interface.c
+Placez le fichier versiofinale.c dans un répertoire de travail. Compilez le code avec la commande suivante :
+gcc `pkg-config --cflags --libs gtk+-3.0` -o visualiseur versiofinale.c
+Exécuter le programme
+Une fois la compilation terminée, lancez le programme avec la commande suivante :
+./visualiseur
+Si tout se passe bien, l'interface graphique s'ouvrira.
+vous trouvrer une video de l'interface de ce programme dans le lien suivant :
+
